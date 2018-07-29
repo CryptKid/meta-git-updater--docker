1e3c1913232b89da7147090f8ee57d20089b3373
CryptKid <CryptKid@users.noreply.github.com>
2018-07-29 12:00:49 +0200

    Now also sends status to stdout for manual usage or own logging purposes

--- meta-git-updater.sh
+++ meta-git-updater.sh
@@ -2,8 +2,9 @@
 directory="/opt/" #The folder, the script should search in for gits
 logfile="/opt/git-pull.log" #A text file, the output goes to
 echo "" >> $logfile
-echo "Running git-updates @ $(date)" >> $logfile
+echo "Running git-updates @ $(date)" |tee -a $logfile
 for i in $(find $directory -type d); do
     cd $i
-    ls .git >/dev/null 2>/dev/null && echo "Pulling $i" >> $logfile && git pull >> $logfile 2>&1
+    ls .git >/dev/null 2>/dev/null && echo "Pulling $i" |tee -a  $logfile && git pull |tee $logfile 2>&1
 done
+
