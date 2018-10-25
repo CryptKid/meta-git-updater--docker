49e9278a2b0837223991691af62bf04bedf640e6
CryptKid <CryptKid@users.noreply.github.com>
2018-10-25 20:52:02 +0200

    Fixed error with space in file path

--- meta-git-updater.sh
+++ meta-git-updater.sh
@@ -4,7 +4,7 @@ logfile="/opt/git-pull.log" #A text file, the output goes to
 echo "" >> $logfile
 echo "Running git-updates @ $(date)" |tee -a $logfile
 for i in $(find $directory -type d); do
-    cd $i
+    cd "$i"
     ls .git >/dev/null 2>/dev/null && echo "Pulling $i" |tee -a  $logfile && git pull |tee $logfile 2>&1
 done
 
