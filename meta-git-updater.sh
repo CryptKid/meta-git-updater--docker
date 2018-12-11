f72207f21ceea096989f1ee389770351eb731a84
CryptKid <CryptKid@users.noreply.github.com>
2018-12-11 08:39:52 +0100

    Improved performance (a lot)

--- meta-git-updater.sh
+++ meta-git-updater.sh
@@ -3,9 +3,8 @@ directory="/opt/" #The folder, the script should search in for gits
 logfile="/opt/git-pull.log" #A text file, the output goes to
 echo "" >> $logfile
 echo "Running git-updates @ $(date)" |tee -a $logfile
-find $directory -type d |while read folder
+find $directory -type d | grep -Eo '+.*/\.git' |sed 's/.git//g' | sort | uniq |while read -r folder
 	do
-		cd "$folder"
-    		ls .git >/dev/null 2>/dev/null && echo "Pulling $folder" |tee -a  $logfile && git pull |tee $logfile 2>&1
+		cd "$folder" && echo "Pulling $folder" |tee -a  $logfile && git pull |tee $logfile 2>&1
 	done
 
